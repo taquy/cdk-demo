bb8cc304d8c12e709ab10ca8c6167e4a8df10ad1
TA QUY <taquy.se@gmail.com>
2020-06-14 19:29:05 +0700

    add vpc stack

--- lib/test-cdk-stack.ts
+++ lib/test-cdk-stack.ts
@@ -1,8 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import {VpcStack} from "./vpc-stack";
 
 export class TestCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new VpcStack(this, 'VpcStack');
+
+
 
     // The code that defines your stack goes here
   }
