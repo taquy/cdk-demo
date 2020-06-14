7f5b72a645adecc43badc6af4fd30df3a4615645
TA QUY <taquy.se@gmail.com>
2020-06-14 19:44:57 +0700

    fix bug rds stack: DBClusterParameterGroup not found: default.aurora5.6

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,14 +1,20 @@
 import {Construct} from '@aws-cdk/core';
 import * as rds from '@aws-cdk/aws-rds'
 import * as ec2 from '@aws-cdk/aws-ec2'
+import {IParameterGroup} from "@aws-cdk/aws-rds";
 
 export class RdsStack {
     cluster: rds.DatabaseCluster;
 
     constructor(scope: Construct, vpc: ec2.Vpc) {
         // The code that defines your stack goes here
+        const paramGroup = {
+            parameterGroupName: 'default.aurora-postgresql11',
+        } as IParameterGroup;
+
         this.cluster = new rds.DatabaseCluster(scope, 'Database', {
             engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+            parameterGroup: paramGroup,
             masterUser: {
                 username: 'root'
             },
